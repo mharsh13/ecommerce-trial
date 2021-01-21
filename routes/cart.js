ec996cd24abdf12cba3b2df4ddfba2c50a6da976
Harsh Mehta <mharsh13dec@gmail.com>
2021-01-21 20:03:23 +0530

    edit product done!

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const productData = require("./addProducts");
 
-const cartItems=[];
+var cartItems=[];
 var totalPrice=0;
 
 router.get('/cart', (req, res) => {
@@ -23,8 +23,64 @@ if(existingProduct){
 
 totalPrice=totalPrice+Number(product.price);
 
-res.redirect("/cart");
+res.redirect("/products");
 
 });
 
+
+router.post("/remove",function(req,res){
+
+const prodId=req.body.productId;
+const products = productData.products;
+const existingProduct=cartItems.find((p) => p.id===prodId);
+totalPrice=totalPrice-(existingProduct.price*existingProduct.quantity);
+existingProduct.quantity=1;
+cartItems.pop(existingProduct);
+
+res.redirect("/cart");
+});
+
+
+
+router.post("/editProduct", function (req, res) {
+  const prodId = req.body.productId;
+  const products = productData.products;
+  const prodIndex= products.findIndex((p)=>p.id===prodId);
+
+  var product={
+    id: prodId,
+    title: req.body.title,
+    price: Number(req.body.price),
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    quantity: Number(req.body.quantity),
+  };
+  
+  
+  products[prodIndex]=product;
+
+  products.forEach((element)=>element.quantity=1);
+
+  cartItems=[];
+  totalPrice=0;
+
+
+  res.redirect("/products");
+
+});
+
+
+router.post("/delete", function (req, res) {
+  const prodId = req.body.productId;
+  const products = productData.products;
+  const deleteProduct = products.find((p) => p.id === prodId);
+  products.forEach((element)=>element.quantity=1);
+  
+  products.pop(deleteProduct);
+  cartItems=[];
+  totalPrice=0;
+  res.redirect("/products");
+});
+
 exports.router = router;
+
--- routes/editProducts.js
+++ routes/editProducts.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const productData = require("./addProducts");
 
 
-router.get('/editProducts', (req, res) => {
-  res.render('editProducts.ejs',{path:"/editProducts"});
+router.get("/editProducts", (req, res) => {
+  res.render("editProducts.ejs", {
+    path: "/editProducts",
+    products: productData.products,
+  });
 });
+
+
+router.get("/products/edit/:productId", (req, res) => {
+  const prodId = req.params.productId;
+  const products = productData.products;
+  const product = products.find((p) => p.id === prodId);
+  res.render("editProduct.ejs", { path: "/editProducts", product: product });
+});
+
 
 exports.router = router;
