af37273600cb0f6674b07a93002b559c8cbe3a8a
Harsh Mehta <mharsh13dec@gmail.com>
2021-02-01 16:10:39 +0530

    upto orders complete

--- routes/cart.js
+++ routes/cart.js
@@ -3,43 +3,38 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/products");
 
-var cartItems = [];
-var totalPrice = 0;
-
 router.get("/cart", (req, res) => {
-  res.render("cart.ejs", {
-    path: "/cart",
-    cartItems: cartItems,
-    totalPrice: totalPrice,
-  });
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then((user) => {
+      const cartItems = user.cart.items;
+      res.render("cart.ejs", {
+        path: "/cart",
+        cartItems: cartItems,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.post("/cart", function (req, res) {
   const prodId = req.body.productId;
-  const products = productData.products;
-  const product = products.find((p) => p.id === prodId);
-  const existingProduct = cartItems.find((p) => p.id === prodId);
-  if (existingProduct) {
-    product.quantity += 1;
-  } else {
-    cartItems.push(product);
-  }
-
-  totalPrice = totalPrice + Number(product.price);
-
-  res.redirect("/products");
+  Product.findById(prodId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => {
+      res.redirect("/products");
+    });
 });
 
 router.post("/remove", function (req, res) {
   const prodId = req.body.productId;
-  const products = productData.products;
-  const existingProduct = cartItems.find((p) => p.id === prodId);
-  totalPrice = totalPrice - existingProduct.price * existingProduct.quantity;
-  existingProduct.quantity = 1;
-  cartItems.pop(existingProduct);
-
-  res.redirect("/cart");
+  req.user.removeFromCart(prodId).then((result) => {
+    res.redirect("/cart");
+  });
 });
-
 
 exports.router = router;
--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const Order = require("../models/order");
 
-router.get('/orders', (req, res) => {
-  res.render('orders.ejs',{path:"/orders"});
+router.get("/orders", (req, res) => {
+  Order.find({ "user.userId": req.user._id }).then((orders) => {
+    res.render("orders.ejs", { path: "/orders", orders: orders});
+  });
+});
+
+router.post("/orders", (req, res) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then((user) => {
+      const cartItems = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: cartItems,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then((result) => {
+      res.redirect("/orders");
+    });
 });
 
 exports.router = router;
