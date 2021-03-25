c538b22b4bf9810a935cc08c9468e5f12707f073
Harsh Mehta <mharsh13dec@gmail.com>
2021-03-26 01:13:02 +0530

    completed!

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,32 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/products");
 
+const ITEMS_PER_PAGE = 3;
+
 router.get("/products", (req, res) => {
-  Product.find().then((products) => {
-    res.render("products.ejs", {
-      path: "/products",
-      products: products,
+  const page = +req.query.page || 1;
+  let totalItems;
+  Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then((products) => {
+      res.render("products.ejs", {
+        path: "/products",
+        products: products,
+        totalProducts: totalItems,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+      });
     });
-  });
 });
 
 exports.router = router;
